cc217dca57db217727964da3fa89178e208ad95b
Ilya Gorev <ilya.gorev.2003@mail.ru>
2023-02-27 23:05:39 +0300

    pizdec

--- js/popup.js
+++ js/popup.js
@@ -3,3 +3,5 @@ const dashboardBtn = document.getElementById("dashboard-button");
 dashboardBtn.addEventListener("click", function() {
     window.open("/html/panel.html");
 });
+
+
--- js/workflow.js
+++ js/workflow.js
@@ -1,5 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // create the canvas for the user interaction
+  //
+  var canvas = new draw2d.Canvas("canvas");
 
-$(document).ready(function () {
-  var button = document.getElementById("add-node-button");
 
+  // create and add two nodes which contains Ports (In and OUT)
+  //
+  var start = new draw2d.shape.node.Start();
+  var end = new draw2d.shape.node.End();
+
+  // ...add it to the canvas
+  canvas.add(start, 50, 250);
+  canvas.add(end, 630, 250);
 });
