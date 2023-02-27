f4fa73cc52c719bdfb2fda1d30a52126bb9bbe41
Ilya Gorev <ilya.gorev.2003@mail.ru>
2023-02-27 20:44:26 +0300

    add base pages

---
+++ js/popup.js
@@ -0,0 +1,5 @@
+const dashboardBtn = document.getElementById("dashboard-button");
+
+dashboardBtn.addEventListener("click", function() {
+    window.open("/html/panel.html");
+});
---
+++ js/workflow.js
@@ -0,0 +1,5 @@
+
+$(document).ready(function () {
+  var button = document.getElementById("add-node-button");
+
+});
